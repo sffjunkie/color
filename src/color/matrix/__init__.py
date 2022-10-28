684ac5c29c328e071dc77a0a076273bac901c4e9
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-28 16:25:26 +0100

    Added matrix as a submodule

--- src/color/matrix/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Matrix
-from .matrix1x import Matrix1x2, Matrix1x3
-from .matrix2x import Matrix2x1, Matrix2x2
-from .matrix3x import Matrix3x1, Matrix3x3
-
-__all__ = [Matrix1x2, Matrix1x3, Matrix2x1, Matrix2x2, Matrix3x1, Matrix3x3, Matrix]
